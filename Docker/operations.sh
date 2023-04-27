52d36d010dcad347704ae1951c5bbe036a7d6b56
Manuel Castro <manuelcastro2021@fau.edu>
2023-04-27 15:14:39 -0400

    Implement logic backend with docker

--- Docker/operations.sh
+++ Docker/operations.sh
@@ -3,12 +3,23 @@
 set -xe
 
 docker build . -t watermark-python
-docker run -it --name watermark watermark-python  
+docker run -i --name watermark watermark-python  
 # python shuffledWatermark.py | tee shuffle.txt
 # python extractWatermark.py | tee extract.txt
 
-docker cp watermark:app/shuffle.txt '/c/Users/mhcd1/OneDrive/Desktop/FAU Classes/06 Spring 2023/ENG 44950  Engineer design/Watermark-Group-Project/WEB-APP/Docker/videos'
-docker cp watermark:app/extract.txt '/c/Users/mhcd1/OneDrive/Desktop/FAU Classes/06 Spring 2023/ENG 44950  Engineer design/Watermark-Group-Project/WEB-APP/Docker/videos'
-docker cp watermark:app/watermarkedVideoA.mp4 '/c/Users/mhcd1/OneDrive/Desktop/FAU Classes/06 Spring 2023/ENG 44950  Engineer design/Watermark-Group-Project/WEB-APP/Docker/videos'
-docker cp watermark:app/watermarkedVideoB.mp4 '/c/Users/mhcd1/OneDrive/Desktop/FAU Classes/06 Spring 2023/ENG 44950  Engineer design/Watermark-Group-Project/WEB-APP/Docker/videos'
-docker cp watermark:app/shuffledWatermarkOutput.mp4 '/c/Users/mhcd1/OneDrive/Desktop/FAU Classes/06 Spring 2023/ENG 44950  Engineer design/Watermark-Group-Project/WEB-APP/Docker/videos'
+#docker cp watermark:app/shuffle.txt '/c/Users/mhcd1/OneDrive/Desktop/FAU Classes/06 Spring 2023/ENG 44950  Engineer design/Watermark-Group-Project/WEB-APP/Docker/videos'
+#docker cp watermark:app/extract.txt '/c/Users/mhcd1/OneDrive/Desktop/FAU Classes/06 Spring 2023/ENG 44950  Engineer design/Watermark-Group-Project/WEB-APP/Docker/videos'
+docker cp watermark:app/extract.txt ../public/videos
+# docker cp watermark:app/watermarkedVideoA.mp4 '/c/Users/mhcd1/OneDrive/Desktop/FAU Classes/06 Spring 2023/ENG 44950  Engineer design/Watermark-Group-Project/WEB-APP/Docker/videos'
+# docker cp watermark:app/watermarkedVideoB.mp4 '/c/Users/mhcd1/OneDrive/Desktop/FAU Classes/06 Spring 2023/ENG 44950  Engineer design/Watermark-Group-Project/WEB-APP/Docker/videos'
+docker cp watermark:app/watermarkedVideoA.mp4 ../public/videos
+docker cp watermark:app/watermarkedVideoB.mp4 ../public/videos
+# docker cp watermark:app/shuffledWatermarkOutput.mp4 '/c/Users/mhcd1/OneDrive/Desktop/FAU Classes/06 Spring 2023/ENG 44950  Engineer design/Watermark-Group-Project/WEB-APP/Docker/videos'
+docker cp watermark:app/shuffledWatermarkOutput.mp4 ../public/videos
+docker cp watermark:app/lowWatermarkedVideoB.mp4 ../public/videos
+docker cp watermark:app/lowWatermarkedVideoA.mp4 ../public/videos
+#docker cp watermark:app/. ../public/videos
+docker stop watermark
+docker container rm watermark
+
+exit
--- server.js
+++ server.js
@@ -2,16 +2,89 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const fileUpload = require('express-fileupload')
+
+const path = require('path');
+
+const cors = require('cors');
+app.use(cors());
+
+var cp = require('cp')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('./public'));
+app.use(fileUpload())
 
-app.get('/', (req, res)=> {
-    res.send(__dirname+'index.html')
+app.get('/', (req, res) => {
+    res.send(__dirname + 'index.html')
 });
+
+app.post('/upload-video', async function (req, res) {
+
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).json({ message: 'No files were uploaded.' });
+    }
+
+    let myFile = req.files.myFile;
+
+    await myFile.mv('./public/videos/input.mp4', async (err) => {
+        if (err) {
+            return res.status(500).json({ message: err });
+        }
+
+        //res.json({ message: 'File uploaded!' });
+        cp.sync('./public/videos/input.mp4', './Docker/app/input.mp4')
+        await x();
+       //res.status(200)
+       //await 
+    });
+
+    //  connect the docker
+    res.redirect('/');
+    
+})
+
+
+async function x() {
+    const { exec } = require("child_process");
+
+    exec("cd Docker && sh operations.sh", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+
+
+    // const { spawn } = require("child_process");
+
+    // const ls = spawn("ls", ["-la"]);
+
+    // ls.stdout.on("data", data => {
+    //     console.log(`stdout: ${data}`);
+    // });
+
+    // ls.stderr.on("data", data => {
+    //     console.log(`stderr: ${data}`);
+    // });
+
+    // ls.on('error', (error) => {
+    //     console.log(`error: ${error.message}`);
+    // });
+
+    // ls.on("close", code => {
+    //     console.log(`child process exited with code ${code}`);
+    // });
+
+}
 
 port = process.env.PORT || 3000
 var listener = app.listen(port); //start the server
-console.log('Server is running on Port: ',port);
+console.log('Server is running on Port: ', port);
 

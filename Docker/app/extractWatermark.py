52a75e7ddd30811018b02518397698971dfeda86
spotter <wilcomeep123@gmail.com>
2023-08-02 14:06:30 -0400

    refactor, increased efficiency of ocr.

--- Docker/app/extractWatermark.py
+++ Docker/app/extractWatermark.py
@@ -1,87 +1,107 @@
 import cv2
 import pytesseract
 import json
+import subprocess
+import numpy as np
 
-# Open video file
-cap = cv2.VideoCapture('shuffledWatermarkOutput.mp4')
- 
-# total frame count
-totalFrames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-
-# current frag time length.
-fragTimeInterval = 1
+out_ts_list = []
+with open("frags/out.m3u8", "r") as file:
+    lines = file.readlines()
+    for line in lines:
+        if ".ts" in line:
+            sanitized_line = line.replace(" ", "").strip() 
+            out_ts_list.append(sanitized_line)
 
 with open("db.json", "r") as infile:
     db = json.load(infile)
 
-userComboTimeLoc = 0
+output_dir = "frags"
+
+totalFramesTested = 0
+
+userListLoc = 0
 
 successCounter = 0
 
-frameCounter = 0
+size_ratio = 3
+
+failed_attempts = 0
 
 userCombo = db["user1"]
 
-prevTime = 0
+fps = db["fps"]
 
-# Loop through video frames, issue with the stupid ts file, sometimes it doesn't round to 1 second, subtracting 8 frames 
-# will give us EDGE CASE safety.
-while cap.isOpened() and cap.get(cv2.CAP_PROP_POS_FRAMES) < totalFrames - 7:
-    ret, frame = cap.read()
+with open('log.txt', 'w') as file:
+        file.write(f" ")
 
-    frameCounter += 1
+for ts_file in out_ts_list:
 
-    if(frameCounter % 20 != 0):
+    try:
+        completed_process = subprocess.run(f"ffmpeg -y -i frags/{ts_file} -c:v copy -c:a copy temp_ts.mp4", shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        userListLoc += 1
+        with open('ts_log.txt', 'a') as file:
+            file.write(str(e)+ "\n")
         continue
     
-    height, width, _ = frame.shape
-    roi = frame[0:int(height/8), 0:int(width/10)]
+    cap = cv2.VideoCapture('temp_ts.mp4')
+ 
+    totalFrames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-    # Apply image processing to enhance text visibility, who know why this works
-    gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (3, 3), 0)
-    thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-
-    # Extract text from frame using OCR
-    text = pytesseract.image_to_string(thresh)
-
-    # Display text on frame
-    cv2.putText(frame, text, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-    # Show frame
-    #cv2.imshow('Frame', roi)
-
-    currentTime = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000.0
+    frameCounter = 0
     
-    # we need to make 10 second intervals later down the road.
-    if(int(currentTime) > int(prevTime) and currentTime != 0):
-        userComboTimeLoc += 1
+    while cap.isOpened() and cap.get(cv2.CAP_PROP_POS_FRAMES) < totalFrames - 7:
+        ret, frame = cap.read()
 
-    if (userCombo[userComboTimeLoc].lower() in text.lower()):
-        successCounter += 1
-    else:
-        #logs to see wtf is happening, outputted to log.txt
-        with open('log.txt', 'a') as file:
-        # Write string to the file[]
-            file.write(f"--------TEXT START-----failed for {userCombo[userComboTimeLoc]} , location {userComboTimeLoc} -\n" + text + "--------- TEXT END ----------- \n")
+        frameCounter += 1
 
-    #print()
-    prevTime = currentTime
+        if(frameCounter % int(fps/2) != 0):
+            continue
+        
+        totalFramesTested += 1
 
-    # Exit if 'q' key is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        height, width, _ = frame.shape
+        
+        roi = frame[0:int(height/size_ratio), 0:int(width/size_ratio)]
 
-# print the success value divided by the total number of frames
-# 98% (it's 100% pretty much for 420p all the way up to 1080p)
-print("Successful extraction rate: {:.2f}%".format(successCounter/(totalFrames / 20) * 100))
+        gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(gray, (3, 3), 0)
+        thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+        text = pytesseract.image_to_string(thresh)
 
+        cv2.putText(frame, text, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+        # cv2.imshow('Frame', roi)
+
+        currentTime = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000.0
+
+        if (userCombo[userListLoc].lower() in text.lower()):
+            successCounter += 1
+        else:
+            if(size_ratio < 7):
+                failed_attempts += 1
+                if(failed_attempts == 7):
+                        failed_attempts = 0
+                        size_ratio += 1
+            with open('log.txt', 'a') as file:
+                file.write(f"--------TEXT START-----failed for {userCombo[userListLoc]} , location {userListLoc} -\n" + text + "--------- TEXT END ----------- \n")
+
+        prevTime = currentTime
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    userListLoc += 1
+
+with open('log.txt', 'a') as file:
+    file.write("Successful extraction rate: {:.2f}%".format((successCounter/totalFramesTested) * 100))
 
 db["user1"] = []
+
+db["fps"] = 0
 
 with open("db.json", "w") as outfile:
     json.dump(db, outfile)
 
-# Release video file and close window
 cap.release()
 cv2.destroyAllWindows()
--- Docker/app/shuffledWatermark.py
+++ Docker/app/shuffledWatermark.py
@@ -1,71 +1,78 @@
 import subprocess
-import uuid
 import cv2
-import pytesseract
 import json
 import random
-import ffmpeg
+import os
 
-# getting the video length
-cap = cv2.VideoCapture('input.mp4')
+input_video = "input4.mp4"
+
+output_dir = "frags"
+
+older_exists = os.path.exists(output_dir)
+if(older_exists):
+    subprocess.run(f"rm -rf ./{output_dir}", shell=True)
+
+os.makedirs(output_dir, exist_ok=True)
+
+cap = cv2.VideoCapture(input_video)
 total_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
 fps = cap.get(cv2.CAP_PROP_FPS)
 duration = total_frames / fps
 
-# some distinct watermark
-textOverlayA = 'XYZ'
+get_resolution = f'ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of json {input_video}'
+result = subprocess.run(get_resolution, shell=True, capture_output=True, text=True)
+resolution_data = json.loads(result.stdout)
 
-# manuel, change the input here based on the outputted resolultion from above.
-# Define FFmpeg command
-applyWatermarkA = (f'ffmpeg -y -i input.mp4 -filter_complex '
-              f'"[0:v]drawtext=fontfile=arial.ttf:fontsize=45*trunc(h/720):'
-              f'text=\'{textOverlayA}\':x=10:y=10:fontcolor=white@1.0:box=1:boxcolor=black@1.0,'
-              f'format=yuva444p[text];[0:v][text]overlay=10:10"'
-              f' -c:a copy watermarkedVideoA.mp4')
+width = resolution_data["streams"][0]["width"]
+height = resolution_data["streams"][0]["height"]
+    
+reference_resolution = (width, height)
 
+textOverlayA = "ABC"
+textOverlayB = "UVW"
+
+applyWatermarkA = (
+    f'ffmpeg -y -i {input_video} -filter_complex '
+    f'"[0:v]drawtext=fontfile=arial.ttf:fontsize={40}:'
+    f'text=\'{textOverlayA}\':x=10:y=10:fontcolor=white@1.0:box=1:boxcolor=black@01.0:'
+    f'boxborderw=8,format=yuva444p[text];[0:v][text]overlay=10:10"'
+    f' -c:a copy watermarkedVideoA.mp4'
+)
 subprocess.run(applyWatermarkA, shell=True)
 
-textOverlayB = 'UVW'
-
-# Define FFmpeg command
-applyWatermarkB = (f'ffmpeg -y -i input.mp4 -filter_complex '
-              f'"[0:v]drawtext=fontfile=arial.ttf:fontsize=45*trunc(h/720):'
-              f'text=\'{textOverlayB}\':x=10:y=10:fontcolor=white@1.0:box=1:boxcolor=black@1.0,'
-              f'format=yuva444p[text];[0:v][text]overlay=10:10"'
-              f' -c:a copy watermarkedVideoB.mp4')
-
-# Run FFmpeg command
+applyWatermarkB = (
+    f'ffmpeg -y -i {input_video} -filter_complex '
+    f'"[0:v]drawtext=fontfile=arial.ttf:fontsize={40}:'
+    f'text=\'{textOverlayB}\':x=10:y=10:fontcolor=white@1.0:box=1:boxcolor=black@1.0:'
+    f'boxborderw=8,format=yuva444p[text];[0:v][text]overlay=10:10"'
+    f' -c:a copy watermarkedVideoB.mp4'
+)
 subprocess.run(applyWatermarkB, shell=True)
 
-# maneul, use this preset for higher res
-#highestResolutionB = f'ffmpeg -i watermarkedVideoB.mp4 -vf "scale=1920:1080:force_original_aspect_ratio=decrease" -c:a copy highWatermarkedVideoB.mp4'
+# # maneul, use this preset for higher res
+# highestResolutionB = f'ffmpeg -y -i watermarkedVideoB.mp4 -vf "scale=1920:1080:force_original_aspect_ratio=decrease" -c:a copy highWatermarkedVideoB.mp4'
 
-#subprocess.run(highestResolutionB, shell=True)
+# subprocess.run(highestResolutionB, shell=True)
 
-# maneul, use this preset for higher res
-#highestResolutionA = f'ffmpeg -i watermarkedVideoA.mp4 -vf "scale=1920:1080:force_original_aspect_ratio=decrease" -c:a copy highWatermarkedVideoA.mp4'
+# # maneul, use this preset for higher res
+# highestResolutionA = f'ffmpeg -y -i watermarkedVideoA.mp4 -vf "scale=1920:1080:force_original_aspect_ratio=decrease" -c:a copy highWatermarkedVideoA.mp4'
 
-#subprocess.run(highestResolutionA, shell=True)
+# subprocess.run(highestResolutionA, shell=True)
 
 # manuel, use this preset for lower res
 lowestResolutionB = f'ffmpeg -y -i watermarkedVideoB.mp4 -vf "scale=-2:420:force_original_aspect_ratio=decrease" lowWatermarkedVideoB.mp4'
-
 subprocess.run(lowestResolutionB, shell=True)
 
 # manuel, use this preset for lower res
 lowestResolutionA = f'ffmpeg -y -i watermarkedVideoA.mp4 -vf "scale=-2:420:force_original_aspect_ratio=decrease" lowWatermarkedVideoA.mp4'
-
-
 subprocess.run(lowestResolutionA, shell=True)
 
 
 # split the input file into fMP4 segments using FFmpeg
-fragmentA = f'ffmpeg -y -i watermarkedVideoA.mp4 -c:v h264 -flags +cgop -g 30 -hls_time 1 fragA.m3u8'
+fragmentA = f'ffmpeg -y -i watermarkedVideoA.mp4 -c:v h264 -flags +cgop -g 30 -hls_time 1 {output_dir}/fragA.m3u8'
 subprocess.run(fragmentA, shell=True)
 
-fragmentB = f'ffmpeg -y -i watermarkedVideoB.mp4 -c:v h264 -flags +cgop -g 30 -hls_time 1 fragB.m3u8'
-
-# select a frag of random type A or B
+fragmentB = f'ffmpeg -y -i watermarkedVideoB.mp4 -c:v h264 -flags +cgop -g 30 -hls_time 1 {output_dir}/fragB.m3u8'
 subprocess.run(fragmentB, shell=True)
 
 with open("db.json", "r") as infile:
@@ -86,11 +93,12 @@ for i in range(0,int(duration)):
     userCombo.append(selectChoice)
     frags.append(selectedFragment)
 
+db["fps"] = fps
+
 with open("db.json", "w") as outfile:
     json.dump(db, outfile)
 
-
-with open("out.m3u8", "w") as f:
+with open(f"{output_dir}/out.m3u8", "w") as f:
     f.write("#EXTM3U\n")
     f.write("#EXT-X-VERSION:3\n")
     f.write("#EXT-X-MEDIA-SEQUENCE:0\n")
@@ -103,6 +111,6 @@ with open("out.m3u8", "w") as f:
     f.write("#EXT-X-ENDLIST\n")
 f.close()
 
-shuffleFrags = f'ffmpeg -y -protocol_whitelist "file,http,https,tcp,tls" -i out.m3u8 -c copy shuffledWatermarkOutput.mp4'
-subprocess.run(shuffleFrags, shell=True)
 
+shuffleFrags = f'ffmpeg -y -protocol_whitelist "file,http,https,tcp,tls" -i  {output_dir}/out.m3u8 -c copy shuffledWatermarkOutput.mp4'
+subprocess.run(shuffleFrags, shell=True)
